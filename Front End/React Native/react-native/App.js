828df980aedd84ff0c6eabb61e055e7f1b45ea72
Vivian Quan <vivianquan@Vivians-MacBook-Pro.local>
2020-03-06 10:18:56 -0800

    [prog]: Added a component folder to refactor code in App

--- Front End/React Native/react-native/App.js
+++ Front End/React Native/react-native/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
 import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+import ColorBox from './components/ColorBox';
 
 const App = () => {
   return (
     <SafeAreaView>
       <View style={styles.container}>
-        <Text style={styles.text}>Here are some boxes of different colors:</Text>
-        <View style={[styles.box, styles.cyan]}>
-          <Text style={styles.boxText}>Cyan: #2aa198</Text>
-        </View>
-        <View style={[styles.box, styles.blue]}>
-          <Text style={styles.boxText}>Blue: #268bd2</Text>
-        </View>
-        <View style={[styles.box, styles.magenta]}>
-          <Text style={styles.boxText}>Magenta: #d33682</Text>
-        </View>
-        <View style={[styles.box, styles.orange]}>
-          <Text style={styles.boxText} >Orange: bd4b16</Text>
-        </View>
+        <Text style={styles.text}>Here are some boxes of different colors: </Text>
+        <ColorBox colorName="Cyan" hexCode="#2aa198" />
+        <ColorBox colorName="Blue" hexCode="#268bd2" />
+        <ColorBox colorName="Magenta" hexCode="#d33682" />
+        <ColorBox colorName="Orange" hexCode="#bd4b16" />
       </View>
     </SafeAreaView>
   );
@@ -32,29 +25,6 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
     marginBottom: 10,
-  },
-  box: {
-    padding: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom: 10,
-    borderRadius: 3,
-  },
-  cyan: {
-    backgroundColor: '#2aa198',
-  },
-  blue: {
-    backgroundColor: '#268bd2',
-  },
-  magenta: {
-    backgroundColor: '#d33682',
-  },
-  orange: {
-    backgroundColor: '#cb4b16',
-  },
-  boxText: {
-    color: 'white',
-    fontWeight: 'bold'
   }
 });
 
