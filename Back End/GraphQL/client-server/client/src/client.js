f0285e21b481c335b2bacc5f1067de7de5cb748d
Vivian Quan <viviannquan@gmail.com>
2020-04-04 23:31:31 -0700

    [prog]: Initalize client

--- Back End/GraphQL/client-server/client/src/client.js
+++ Back End/GraphQL/client-server/client/src/client.js
@@ -6,3 +6,14 @@ import gql from 'graphql-tag'
 /**
  * Create a new apollo client and export as default
  */
+
+// Link - network interface to access graphQL server ; telling our Apollo are API is here
+const link = new HttpLink({ uri: 'http://localhost:4000/' });
+const cache = new InMemoryCache();
+
+// initializing the client
+const client = new ApolloClient({
+  link, cache
+});
+
+export default client;
