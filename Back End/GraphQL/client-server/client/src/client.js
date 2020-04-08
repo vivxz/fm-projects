f2882597c8ca12dc543400c47fcd6cd3d4492a1f
Vivian Quan <viviannquan@gmail.com>
2020-04-07 21:14:17 -0700

    [prog]: Created a client side schema

--- Back End/GraphQL/client-server/client/src/client.js
+++ Back End/GraphQL/client-server/client/src/client.js
@@ -9,6 +9,20 @@ import gql from 'graphql-tag'
  * Create a new apollo client and export as default
  */
 
+const typeDefs = gql`
+ extend type User {
+   type: Int
+ }
+`
+
+const resolvers = {
+  User: {
+    age() {
+      return 35
+    }
+  }
+}
+
 // Link - network interface to access graphQL server ; telling our Apollo are API is here
 const http = new HttpLink({ uri: 'http://localhost:4000/' });
 const delay = setContext(
@@ -28,7 +42,7 @@ const cache = new InMemoryCache();
 
 // initializing the client
 const client = new ApolloClient({
-  link, cache
+  link, cache, typeDefs, resolvers
 });
 
 export default client;
