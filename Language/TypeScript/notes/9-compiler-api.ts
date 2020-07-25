194f8144afce7c7775e9e3f2778496275c7ff495
Vivian Quan <viviannquan@gmail.com>
2020-07-25 15:04:17 -0700

    [prog]: Edited notes for compiler

--- Language/TypeScript/notes/9-compiler-api.ts
+++ Language/TypeScript/notes/9-compiler-api.ts
@@ -1,5 +1,5 @@
 import * as path from "path";
-import * as ts from "typescript";
+import * as ts from "typescript"; // wild card import to get everything from typescript
 
 function isDefined<T>(x: T | undefined): x is T {
   return typeof x !== "undefined";
@@ -9,52 +9,53 @@ function isDefined<T>(x: T | undefined): x is T {
 const program = ts.createProgram({
   options: {
     target: ts.ScriptTarget.ESNext
-  },
+  }, // equivalent to ts.config
   rootNames: [
     // path to ../examples/hello-ts/src/index.ts
     path.join(__dirname, "..", "examples", "hello-ts", "src", "index.ts")
   ]
 });
 
-// // (2) Get the non-declaration (.d.ts) source files (.ts)
-// const nonDeclFiles = program
-//   .getSourceFiles()
-//   .filter(sf => !sf.isDeclarationFile);
+// (2) Get the non-declaration (.d.ts) source files (.ts)
+const nonDeclFiles = program 
+  .getSourceFiles()
+  .filter(sf => !sf.isDeclarationFile); // filtering out dts files
 
-// // (3) get the type-checker
-// const checker = program.getTypeChecker();
+// (3) get the type-checker –– takes all types and interface and binds them together and see what is going to work
+const checker = program.getTypeChecker();
 
-// /**
-//  * (4) use the type checker to obtain the
-//  * -   appropriate ts.Symbol for each SourceFile
-//  */
-// const sfSymbols = nonDeclFiles
-//   .map(f => checker.getSymbolAtLocation(f))
-//   .filter(isDefined); // here's the type guard to filter out undefined
+/**
+ * (4) use the type checker to obtain the
+ * -   appropriate ts.Symbol for each SourceFile
+ */
+const sfSymbols = nonDeclFiles
+  .map(f => checker.getSymbolAtLocation(f)) // location of file
+  .filter(isDefined); // here's the type guard to filter out undefined
 
-// // (5) for each SourceFile Symbol
-// sfSymbols.forEach(sfSymbol => {
-//   const { exports: fileExports } = sfSymbol;
-//   console.log(sfSymbol.name);
-//   if (fileExports) {
-//     // - if there are exports
-//     console.log("== Exports ==");
-//     fileExports.forEach((value, key) => {
-//       // - for each export
-//       console.log(
-//         key, // - log its name
+// (5) for each SourceFile Symbol
+sfSymbols.forEach(sfSymbol => {
+  const { exports: fileExports } = sfSymbol; 
+  console.log(sfSymbol.name); // log out each symbols name
+  if (fileExports) {
+    // - if there are exports
+    console.log("== Exports ==");
+    fileExports.forEach((value, key) => {
+      // - for each export
+      console.log(
+        key, // - log its name
 
-//         // - and its type (stringified)
-//         checker.typeToString(checker.getTypeAtLocation(value.valueDeclaration))
-//       );
-//       const jsDocTags = value.getJsDocTags();
-//       if (jsDocTags.length > 0) {
-//         // - if there are JSDoc comment tags
-//         console.log(
-//           // - log them out as key-value pairs
-//           jsDocTags.map(tag => `\t${tag.name}: ${tag.text}`).join("\n")
-//         );
-//       }
-//     });
-//   }
-// });
+        // - and its type (stringified)
+        checker.typeToString(checker.getTypeAtLocation(value.valueDeclaration))
+      );
+      const jsDocTags = value.getJsDocTags();
+      // getting it into a form that is readable
+      if (jsDocTags.length > 0) {
+        // - if there are JSDoc comment tags
+        console.log(
+          // - log them out as key-value pairs
+          jsDocTags.map(tag => `\t${tag.name}: ${tag.text}`).join("\n")
+        );
+      }
+    });
+  }
+});
